fd108400a2318bf29d06da09424285fa1774e90b
gauravgit-dell <2REACHGPR@GMAIL.COM>
2025-01-29 11:43:56 +0530

    Update and rename program1.py to program1Act1.py

--- LAB 1/program1Act1.py
+++ LAB 1/program1Act1.py
@@ -2,13 +2,27 @@
 arr =['10','89','9','56','4','80','8']
 mini = 10
 maxi = 10
+secmaxi = 10
+secmini = 10
 print(arr)
 for num in arr:
     num = int(num)
  
     if num < mini:
+        secmini = mini
         mini=num
     if num > maxi:
+        secmaxi=maxi
         maxi=num
+    elif num > secmaxi and num !=maxi:
+        secmaxi = num
+    elif num < secmini and num != mini:
+        secmini = num
+    
+        
 print("smallest element is ",mini)
 print("largest element is ",maxi)
+print("second largest is",secmaxi)
+print("second smallest element",secmini)
+
+ 
